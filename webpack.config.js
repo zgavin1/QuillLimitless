fdc140488d6dd23258d893fdc6d23c05cad8b312
Donald McKendrick <ddmckendrick@gmail.com>
2015-11-29 11:38:34 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["react-hot", "babel-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query:
+          {
+            presets:['es2015','react']
+          }
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      }
+    ],
+  },
+  context: __dirname + "/app",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html",
+  },
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+}
