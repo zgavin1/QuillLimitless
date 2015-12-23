da469f87916b1bafce51c8a06b261a622f695e02
Donald McKendrick <ddmckendrick@gmail.com>
2015-12-23 18:21:13 -0500

    Mark submission as correct or incorrect.

--- app/components/question/question.jsx
+++ app/components/question/question.jsx
@@ -1,15 +1,107 @@
 import React from "react";
 
+import "../../styles/question.scss";
+
 export default React.createClass({
-  updateScore: function(){
-    this.props.action("INCREMENT")
+  updateScore: function (direction, index) {
+    this.props.action(direction, index);
+  },
+
+  splitPrompt: function () {
+    return this.props.data.question.question.prompt.split(" ");
+  },
+
+  getTargetWord: function () {
+    const words = this.splitPrompt();
+    let j = 0;
+    for (let i = 0; i < words.length; i++) {
+      if (words[i][0] === "{") {
+        j = i;
+        break;
+      }
+    }
+    return j;
+  },
+
+  checkWord: function (index) {
+    if (index === this.getTargetWord()) {
+      this.updateScore("INCREMENT", index);
+    } else {
+      this.updateScore("DECREMENT", index);
+    }
+  },
+
+  beforeAnsweringWordList: function () {
+    const words = this.splitPrompt();
+    this.getTargetWord();
+    return words.map((word, index) => {
+      const cb = (i) => {
+        const ind = i;
+        const call = () => {
+          this.checkWord(ind);
+        }
+        return call;
+      }
+      return <span onClick={cb(index)}>{word.replace(/[{}]/g, "")} </span>
+    });
+  },
+
+  answeredCorrectlyWordList: function () {
+    const words = this.splitPrompt();
+    const correctIndex = this.getTargetWord();
+    return words.map((word, index) => {
+      if (index === correctIndex) {
+        return <span className="correct">{word.replace(/[{}]/g, "")} </span>
+      } else {
+        return <span>{word.replace(/[{}]/g, "")} </span>
+      }
+
+    });
+  },
+
+  answeredIncorrectlyWordList: function () {
+    const words = this.splitPrompt();
+    const correctIndex = this.getTargetWord();
+    return words.map((word, index) => {
+      if (index === correctIndex) {
+        return <span className="correct">{word.replace(/[{}]/g, "")} </span>
+      }
+      else if (index === this.props.data.index) {
+        return <span className="incorrect">{word.replace(/[{}]/g, "")} </span>
+      }
+      else {
+        return <span>{word.replace(/[{}]/g, "")} </span>
+      }
+
+    });
+  },
+
+  generateWordList: function () {
+    if (this.props.data.correct === true) {
+      return this.answeredCorrectlyWordList();
+    }
+    else if (this.props.data.correct === false) {
+      return this.answeredIncorrectlyWordList();
+    } else {
+      return this.beforeAnsweringWordList();
+    }
+  },
+
+  stateSpecificComponent: function () {
+    if (this.props.data.correct === true) {
+      return <h4>Correct</h4>
+    }
+    else if (this.props.data.correct === false) {
+      return <h4>Incorrect</h4>
+    }
   },
 
   render: function () {
     return (
       <div>
         <h2>Question</h2>
-        <p onClick={this.updateScore}>{this.props.data}</p>
+        {this.stateSpecificComponent()}
+        <p>{this.generateWordList()}</p>
       </div>
     )
   }
--- app/components/root.js
+++ app/components/root.js
@@ -7,18 +7,36 @@ import "../styles/style.scss";
 export default React.createClass({
   getInitialState: function(){
     this.state = {};
-    this.state.score = 5;
-    this.state.question = "Theirs no place I'd rather live than New York City.";
+    this.state.score = 0;
+    this.state.question = {
+      concept: {
+        name: "Than, Then",
+        conceptName: "Than",
+        standard: "4.1g. Commonly Confused Words,Jzw0qjQwbyM2twtkmW1"
+      },
+      question: {
+        answer: "There are more {than} 300 million people in the United States.",
+        prompt: "There are more {then} 300 million people in the United States."
+      }
+    };
     return this.state;
   },
 
-  updateScore: function(direction){
+  updateScore: function(direction, index){
     switch (direction) {
       case "INCREMENT":
-        this.setState({score: this.state.score + 1});
+        this.setState({
+          score: this.state.score + 1,
+          correct: true,
+          index: index
+        });
         break;
       case "DECREMENT":
-        this.setState({score: this.state.score - 1});
+        this.setState({
+          score: this.state.score - 1,
+          correct: false,
+          index: index
+        });
         break;
       default:
         return
@@ -30,7 +48,7 @@ export default React.createClass({
       <div>
         <h1>Quill Limitless</h1>
         <ScoreBar data={this.state.score}/>
-        <Question data={this.state.question} action={this.updateScore}/>
+        <Question data={this.state} action={this.updateScore}/>
       </div>
     )
   }
