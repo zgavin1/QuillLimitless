92dcf1657f6894dcb36a71a98788d0191b285298
Donald McKendrick <ddmckendrick@gmail.com>
2016-01-05 14:51:31 -0500

    remove commented out code and restore dev tools

--- app/components/root.js
+++ app/components/root.js
@@ -10,67 +10,6 @@ import "../styles/bootstrap.scss";
 import "../styles/style.scss";
 
 class Root extends Component {
-  // getInitialState: function(){
-  //   this.state = {};
-  //   this.state.score = 0;
-  //   this.state.question = {
-  //     concept: {
-  //       name: "Than, Then",
-  //       conceptName: "Than",
-  //       standard: "4.1g. Commonly Confused Words,Jzw0qjQwbyM2twtkmW1"
-  //     },
-  //     question: {
-  //       answer: "There are more {than} 300 million people in the United States.",
-  //       prompt: "There are more {then} 300 million people in the United States."
-  //     }
-  //   };
-  //   return this.state;
-  // },
-  //
-  // updateScore: function(direction, stage, index){
-  //   switch (stage) {
-  //     case 1:
-  //       switch (direction) {
-  //         case "INCREMENT":
-  //           this.setState({
-  //             score: this.state.score + 1,
-  //             correct: true,
-  //             index: index
-  //           });
-  //           break;
-  //         case "DECREMENT":
-  //           this.setState({
-  //             score: this.state.score - 1,
-  //             correct: false,
-  //             index: index
-  //           });
-  //           break;
-  //         default:
-  //           return
-  //       }
-  //       break
-  //     case 2:
-  //       switch (direction) {
-  //         case "INCREMENT":
-  //           this.setState({
-  //             score: this.state.score + 1,
-  //             correct: true,
-  //             index: index,
-  //             finished: true
-  //           });
-  //           break;
-  //         case "DECREMENT":
-  //           this.setState({
-  //             correct: false,
-  //             index: index,
-  //             finished: true
-  //           });
-  //           break;
-  //         default:
-  //           return
-  //       }
-  //   }
-  // },
 
   render(){
     const { dispatch, score, question} = this.props;
@@ -85,7 +24,7 @@ class Root extends Component {
                   }
           dispatch={dispatch}
         />
-        
+        <DevTools />
       </div>
     )
   }
